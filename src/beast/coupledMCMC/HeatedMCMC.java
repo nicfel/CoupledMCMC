d5555c921dad08d7c939c0dc3665613a2c0f97fc
Nicola Müller <nicola.felix.mueller@gmail.com>
2018-10-15 15:10:35 +0200

    initial commit

---
+++ src/beast/coupledMCMC/HeatedMCMC.java
@@ -0,0 +1,312 @@
+package beast.inference;
+
+
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import beast.core.Description;
+import beast.core.Distribution;
+import beast.core.Logger;
+import beast.core.MCMC;
+import beast.core.Operator;
+import beast.core.StateNodeInitialiser;
+import beast.core.util.Evaluator;
+import beast.core.util.Log;
+import beast.util.Randomizer;
+
+@Description("Base class for doing Metropolis coupled MCMC. Each instance represenst a chain at a different temperature.")
+public class HeatedMCMC extends MCMC {
+	
+	// LAMBDA is temperature multiplier
+	static double LAMBDA = 0.01;
+	
+	// temperature on which this chain runs
+	protected double temperature = 1.0;
+	
+	// nr of samples between re-arranging states
+	protected int resampleEvery = 1000;
+	
+	// keep track of total nr of states sampled, using currentSample
+	protected int currentSample = 0;
+
+	protected double getCurrentLogLikelihood() {
+		return oldLogLikelihood / temperature;
+	};
+
+	// set chain number for a given lambda
+	
+	public void setChainNr(int i, int resampleEvery, double lambda) {
+		LAMBDA = lambda; // unneeded step, but still here
+		temperature = 1 + i  * LAMBDA;
+		this.resampleEvery = resampleEvery;
+	}
+	
+	public double getTemperature(){
+		return temperature;
+	}
+	
+//	public void setChainNr(int i, int resampleEvery) {
+//		temperature = 1 + i * LAMBDA;
+//		this.resampleEvery = resampleEvery;
+//	}
+
+
+	protected double calcCurrentLogLikelihoodRobustly() {
+		oldLogLikelihood = robustlyCalcPosterior(posterior);
+		return getCurrentLogLikelihood();
+	};
+
+	
+//	@Override
+//	protected void doLoop() throws Exception {
+//	//	runTillResample();
+//	}
+	
+    @Override
+    public void run() throws IOException, SAXException, ParserConfigurationException {
+        // set up state (again). Other plugins may have manipulated the
+        // StateNodes, e.g. set up bounds or dimensions
+        state.initAndValidate();
+        // also, initialise state with the file name to store and set-up whether to resume from file
+        state.setStateFileName(stateFileName);
+        operatorSchedule.setStateFileName(stateFileName);
+
+        burnIn = burnInInput.get();
+        chainLength = chainLengthInput.get();
+        int nInitialisationAttempts = 0;
+        //state.setEverythingDirty(true);
+        posterior = posteriorInput.get();
+
+        if (restoreFromFile) {
+            state.restoreFromFile();
+            operatorSchedule.restoreFromFile();
+            burnIn = 0;
+            oldLogLikelihood = state.robustlyCalcPosterior(posterior);
+        } else {
+            do {
+                for (final StateNodeInitialiser initialiser : initialisersInput.get()) {
+                    initialiser.initStateNodes();
+                }
+                oldLogLikelihood = state.robustlyCalcPosterior(posterior);
+                nInitialisationAttempts += 1;
+            } while (Double.isInfinite(oldLogLikelihood) && nInitialisationAttempts < numInitializationAttempts.get());
+        }
+        final long startTime = System.currentTimeMillis();
+
+        // do the sampling
+        logAlpha = 0;
+        debugFlag = Boolean.valueOf(System.getProperty("beast.debug"));
+
+//        System.err.println("Start state:");
+//        System.err.println(state.toString());
+
+        System.err.println("Start likelihood: " + oldLogLikelihood + " " + (nInitialisationAttempts > 1 ? "after " + nInitialisationAttempts + " initialisation attempts" : ""));
+        if (Double.isInfinite(oldLogLikelihood) || Double.isNaN(oldLogLikelihood)) {
+            reportLogLikelihoods(posterior, "");
+            throw new RuntimeException("Could not find a proper state to initialise. Perhaps try another seed.");
+        }
+
+        loggers = loggersInput.get();
+
+        // put the loggers logging to stdout at the bottom of the logger list so that screen output is tidier.
+        Collections.sort(loggers, (o1, o2) -> {
+            if (o1.isLoggingToStdout()) {
+                return o2.isLoggingToStdout() ? 0 : 1;
+            } else {
+                return o2.isLoggingToStdout() ? -1 : 0;
+            }
+        });
+        // warn if none of the loggers is to stdout, so no feedback is given on screen
+        boolean hasStdOutLogger = false;
+        boolean hasScreenLog = false;
+        for (Logger l : loggers) {
+        	if (l.isLoggingToStdout()) {
+        		hasStdOutLogger = true;
+        	}
+        	if (l.getID() != null && l.getID().equals("screenlog")) {
+        		hasScreenLog = true;
+        	}
+        }
+        if (!hasStdOutLogger) {
+        	Log.warning.println("WARNING: If nothing seems to be happening on screen this is because none of the loggers give feedback to screen.");
+        	if (hasScreenLog) {
+        		Log.warning.println("WARNING: This happens when a filename  is specified for the 'screenlog' logger.");
+        		Log.warning.println("WARNING: To get feedback to screen, leave the filename for screenlog blank.");
+        		Log.warning.println("WARNING: Otherwise, the screenlog is saved into the specified file.");
+        	}
+        }
+
+        // initialises log so that log file headers are written, etc.
+        for (final Logger log : loggers) {
+            log.init();
+        }
+
+//        doLoop();
+//
+//        System.out.println();
+//        operatorSchedule.showOperatorRates(System.out);
+//
+//        System.out.println();
+//        final long endTime = System.currentTimeMillis();
+//        System.out.println("Total calculation time: " + (endTime - startTime) / 1000.0 + " seconds");
+//        close();
+//
+//        System.err.println("End likelihood: " + oldLogLikelihood);
+////        System.err.println(state);
+//        state.storeToFile(chainLength);
+//        operatorSchedule.storeToFile();
+        //Randomizer.storeToFile(stateFileName);
+    } // run;
+	
+	
+	// run MCMC inner loop for resampleEvery nr of samples
+	protected long runTillResample() throws Exception {
+	       int corrections = 0;
+	       for (int sampleNr = currentSample; sampleNr < currentSample + resampleEvery; sampleNr++) {
+	            final int currentState = sampleNr;
+
+	            state.store(currentState);
+//	            if (m_nStoreEvery > 0 && iSample % m_nStoreEvery == 0 && iSample > 0) {
+//	                state.storeToFile(iSample);
+//	            	operatorSchedule.storeToFile();
+//	            }
+
+	            final Operator operator = operatorSchedule.selectOperator();
+	            //System.out.print("\n" + sampleNr + " " + operator.getName()+ ":");
+
+	            final Distribution evaluatorDistribution = operator.getEvaluatorDistribution();
+	            Evaluator evaluator = null;
+
+	            if (evaluatorDistribution != null) {
+	                evaluator = new Evaluator() {
+	                    @Override
+	                    public double evaluate() {
+	                        double logP = 0.0;
+
+	                        state.storeCalculationNodes();
+	                        state.checkCalculationNodesDirtiness();
+
+	                        try {
+	                            logP = evaluatorDistribution.calculateLogP();
+	                        } catch (Exception e) {
+	                            e.printStackTrace();
+	                            System.exit(1);
+	                        }
+
+	                        state.restore();
+	                        state.store(currentState);
+
+	                        return logP;
+	                    }
+	                };
+	            }
+
+	            final double logHastingsRatio = operator.proposal(evaluator);
+
+	            if (logHastingsRatio != Double.NEGATIVE_INFINITY) {
+
+	            	if (operator.requiresStateInitialisation()) {
+	            		state.storeCalculationNodes();
+	            		state.checkCalculationNodesDirtiness();
+	            	}
+
+	                newLogLikelihood = posterior.calculateLogP();
+
+	                logAlpha = newLogLikelihood/temperature - oldLogLikelihood/temperature + logHastingsRatio; 
+	                //System.out.println(logAlpha + " " + newLogLikelihood + " " + oldLogLikelihood);
+	                if (logAlpha >= 0 || Randomizer.nextDouble() < Math.exp(logAlpha)) {
+	                    // accept
+	                    oldLogLikelihood = newLogLikelihood;
+	                    state.acceptCalculationNodes();
+
+	                    if (sampleNr >= 0) {
+	                        operator.accept();
+	                    }
+	                    //System.out.print(" accept");
+	                } else {
+	                    // reject
+	                    if (sampleNr >= 0) {
+	                        operator.reject(newLogLikelihood == Double.NEGATIVE_INFINITY ? -1 : 0);
+	                    }
+	                    state.restore();
+	                    state.restoreCalculationNodes();
+	                    //System.out.print(" reject");
+	                }
+	                state.setEverythingDirty(false);
+	            } else {
+	                // operation failed
+	                if (sampleNr >= 0) {
+	                    operator.reject(-2);
+	                }
+	                state.restore();
+					if (!operator.requiresStateInitialisation()) {
+	                    state.setEverythingDirty(false);
+	                    state.restoreCalculationNodes();
+					}
+	                //System.out.print(" direct reject");
+	            }
+	            log(sampleNr);
+
+	            if (debugFlag && sampleNr % 3 == 0 || sampleNr % 10000 == 0) {
+	                // check that the posterior is correctly calculated at every third
+	                // sample, as long as we are in debug mode
+	            	final double fNonStochasticLogP = posterior.getNonStochasticLogP();
+	                final double fLogLikelihood = state.robustlyCalcNonStochasticPosterior(posterior);
+	                if (Math.abs(fLogLikelihood - fNonStochasticLogP) > 1e-6) {
+	                    reportLogLikelihoods(posterior, "");
+	                    System.err.println("At sample " + sampleNr + "\nLikelihood incorrectly calculated: " + fNonStochasticLogP + " != " + fLogLikelihood
+	                            + " Operator: " + operator.getClass().getName());
+	                }
+	                if (sampleNr > NR_OF_DEBUG_SAMPLES * 3) {
+	                    // switch off debug mode once a sufficient large sample is checked
+	                    debugFlag = false;
+	                    if (Math.abs(fLogLikelihood - fNonStochasticLogP) > 1e-6) {
+	                        // incorrect calculation outside debug period.
+	                        // This happens infrequently enough that it should repair itself after a robust posterior calculation
+	                        corrections++;
+	                        if (corrections > 100) {
+	                            // after 100 repairs, there must be something seriously wrong with the implementation
+	                            System.err.println("Too many corrections. There is something seriously wrong that cannot be corrected");
+	                            state.storeToFile(sampleNr);
+	                            operatorSchedule.storeToFile();
+	                            System.exit(0);
+	                        }
+	                        oldLogLikelihood = state.robustlyCalcPosterior(posterior);;
+	                    }
+	                } else {
+	                    if (Math.abs(fLogLikelihood - fNonStochasticLogP) > 1e-6) {
+	                        // halt due to incorrect posterior during intial debug period
+	                        state.storeToFile(sampleNr);
+	                        operatorSchedule.storeToFile();
+	                        System.exit(0);
+	                    }
+	                }
+	            } else {
+	                if (sampleNr >= 0) {
+	                	operator.optimize(logAlpha);
+	                }
+	            }
+	            callUserFunction(sampleNr);
+
+	            // make sure we always save just before exiting
+	            if (storeEvery > 0 && (sampleNr + 1) % storeEvery == 0 || sampleNr == chainLength) {
+	                /*final double fLogLikelihood = */
+	                state.robustlyCalcNonStochasticPosterior(posterior);
+	                state.storeToFile(sampleNr);
+	                operatorSchedule.storeToFile();
+	            }
+	        }
+	        if (corrections > 0) {
+	            System.err.println("\n\nNB: " + corrections + " posterior calculation corrections were required. This analysis may not be valid!\n\n");
+	        }
+	        currentSample += resampleEvery;
+	        return System.currentTimeMillis();
+	}
+
+	public void optimiseRunTime(long startTime, long endTime, long endTimeMainChain) {}
+	
+}
---
+++ src/beast/coupledMCMC/MCMCMC.java
@@ -0,0 +1,291 @@
+package beast.inference;
+
+
+import beast.core.*;
+import beast.core.util.Log;
+import beast.util.Randomizer;
+import beast.util.XMLParser;
+import beast.util.XMLProducer;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+// Altekar G, Dwarkadas S, Huelsenbeck J and Ronquist F (2004). 
+// Parallel Metropolis Coupled Markov Chain Monte Carlo For Bayesian Phylogenetic Inference.
+// Bioinformatics, 20. ISSN 1367-4803, 
+// http://dx.doi.org/10.1093/bioinformatics/btg427.
+
+@Citation(value= "Altekar G, Dwarkadas S, Huelsenbeck J and Ronquist F (2004). \n" +
+				"  Parallel Metropolis Coupled Markov Chain Monte Carlo For Bayesian Phylogenetic Inference.\n" +
+				"  Bioinformatics, 20(3), 407-415."
+		, year = 2004, firstAuthorSurname = "Altekar",
+		DOI="10.1093/bioinformatics/btg427")
+@Description("Metropolis-Coupled Markov Chain Monte Carlo" +
+		"" +
+		"Note that log file names should have $(seed) in their name so " +
+		"that the first chain uses the actual seed in the file name and all subsequent chains add one to it." +
+		"Furthermore, the log and tree log should have the same sample frequency.")
+public class MCMCMC extends MCMC {
+	public Input<Integer> nrOfChainsInput = new Input<Integer>("chains", " number of chains to run in parallel (default 2)", 2);
+	public Input<Integer> resampleEveryInput = new Input<Integer>("resampleEvery", "number of samples in between resampling (and possibly swappping) states", 1000);
+	public Input<String> heatedMCMCClassInput = new Input<String>("heatedMCMCClass", "Name of the class used for heated chains", HeatedMCMC.class.getName());
+	public Input<String> tempDirInput = new Input<>("tempDir","directory where temporary files are written","/tmp/");
+	public Input<Double> temperatureScalerInput = new Input<>("temperatureScaler","temperature scaler, the higher this value, the hotter the chains",0.1);
+	public Input<String> stateFileNameInput = new Input<>("stateFileName","name of the state file", "state.backup.xml");
+
+	
+	
+	// nr of samples between re-arranging states
+	int resampleEvery = 10;
+	
+	double temperatureScaler = 0.01;
+	
+	
+	/** plugins representing MCMC with model, loggers, etc **/
+	HeatedMCMC [] chains;
+	/** threads for running MCMC chains **/
+	Thread [] threads;
+	/** keep track of time taken between logs to estimate speed **/
+    long startLogTime;
+
+	// keep track of when threads finish in order to optimise thread usage
+	long [] finishTimes;
+
+	List<StateNode> tmpStateNodes;
+
+	@Override
+	public void initAndValidate() {
+		if (nrOfChainsInput.get() < 1) {
+			throw new RuntimeException("chains must be at least 1");
+		}
+		if (nrOfChainsInput.get() == 1) {
+			Log.warning.println("Warning: MCMCMC needs at least 2 chains to be effective, but chains=1. Running plain MCMC.");
+		}
+		chains = new HeatedMCMC[nrOfChainsInput.get()];
+		
+		resampleEvery = resampleEveryInput.get();
+		
+		temperatureScaler = temperatureScalerInput.get();
+
+		// the difference between the various chains is
+		// 1. it runs an MCMC, not a  MultiplMCMC
+		// 2. remove chains attribute
+		// 3. output logs change for every chain
+		// 4. log to stdout is removed to prevent clutter on stdout
+		
+		String sXML = new XMLProducer().toXML(this);
+		sXML = sXML.replaceAll("chains=['\"][^ ]*['\"]", "");
+		sXML = sXML.replaceAll("heatedMCMCClass=['\"][^ ]*['\"]", "");
+		sXML = sXML.replaceAll("resampleEvery=['\"][^ ]*['\"]", "");
+		sXML = sXML.replaceAll("tempDir=['\"][^ ]*['\"]", "");
+		sXML = sXML.replaceAll("temperatureScaler=['\"][^ ]*['\"]", "");
+		sXML = sXML.replaceAll("stateFileName=['\"][^ ]*['\"]", "");
+
+	
+        String sMCMCMC = this.getClass().getName();
+		while (sMCMCMC.length() > 0) {
+			sXML = sXML.replaceAll("\\b"+MCMCMC.class.getName()+"\\b", heatedMCMCClassInput.get());
+			if (sMCMCMC.indexOf('.') >= 0) {
+				sMCMCMC = sMCMCMC.substring(sMCMCMC.indexOf('.')+1);
+			} else {
+				sMCMCMC = "";
+			}
+		}
+		long nSeed = Randomizer.getSeed();
+		
+		
+
+		
+		
+		// create new chains		
+		for (int i = 0; i < chains.length; i++) {
+			XMLParser parser = new XMLParser();
+			String sXML2 = sXML;
+			sXML2 = sXML2.replaceAll("\\$\\(seed\\)", nSeed+i+"");
+			
+			try {
+		        FileWriter outfile = new FileWriter(new File(tempDirInput.get() + "MCMCMC.xml"));
+		        outfile.write(sXML2);
+		        outfile.close();
+				
+				chains[i] = (HeatedMCMC) parser.parseFragment(sXML2, true);
+	
+				// remove all loggers, except for main chain
+				if (i != 0) {
+					chains[i].loggersInput.get().clear();
+				}
+				String chainFileName = stateFileNameInput.get()  + "." + i + ".state";
+				
+				chains[i].setChainNr(i, resampleEvery, temperatureScalerInput.get());
+				chains[i].setStateFile(chainFileName, restoreFromFile);
+			
+				chains[i].run();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	
+		// reopen log files for main chain, which were closed at the end of run(); 
+		//Logger.FILE_MODE = LogFileMode.resume;
+		//for (Logger logger : m_chains[0].loggersInput.get()) {
+		//	logger.init();
+		//}
+		
+		// get a copy of the list of state nodes to facilitate swapping states
+		tmpStateNodes = startStateInput.get().stateNodeInput.get();
+
+		chainLength = chainLengthInput.get();
+		finishTimes = new long[chains.length];
+	} // initAndValidate
+	
+	
+	
+	class HeatedChainThread extends Thread {
+		final int chainNr;
+		HeatedChainThread(int chainNr) {
+			this.chainNr = chainNr;
+		}
+		public void run() {
+			try {
+				finishTimes[chainNr] = chains[chainNr].runTillResample();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override 
+	public void run() throws IOException {
+		
+		int totalSwaps = 0;
+		int successfullSwaps = 0, successfullSwaps0 = 0;
+//		for (HeatedMCMC chain:m_chains) {
+//			chain.startStateInput.get().setEverythingDirty(true);
+//		}
+		for (int sampleNr = 0; sampleNr < chainLength; sampleNr += resampleEvery) {
+			long startTime = System.currentTimeMillis();
+			
+			// start threads with individual chains here.
+			threads = new Thread[chains.length];
+			
+			for (int k = 0; k < chains.length; k++) {
+				threads[k] = new HeatedChainThread(k);
+				threads[k].start();
+			}
+
+			// wait for the chains to finish
+	        startLogTime = System.currentTimeMillis();
+			for (Thread thread : threads) {
+				try {
+					thread.join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if (chains.length > 1) {
+//			for (int dummy = 1; dummy < chains.length; dummy++){
+				// resample state
+				int i = Randomizer.nextInt(chains.length);
+				int j = i;
+				while (i == j) {
+					j = Randomizer.nextInt(chains.length);
+				}
+				if (i > j) {
+					int tmp = i; i = j; j = tmp;
+				}
+				
+				
+				double p1before = chains[i].getCurrentLogLikelihood();
+				double p2before = chains[j].getCurrentLogLikelihood();
+//				swapStates(chains[i], chains[j]);
+//				double p1after = chains[i].calcCurrentLogLikelihoodRobustly();
+//				double p2after = chains[j].calcCurrentLogLikelihoodRobustly();
+				// robust calculations can be extremly expensive, just calculate the new probs instead 
+				double p1after = chains[i].getCurrentLogLikelihood() * chains[i].getTemperature() / chains[j].getTemperature();
+				double p2after = chains[j].getCurrentLogLikelihood() * chains[j].getTemperature() / chains[i].getTemperature();
+
+				
+//				System.out.println(p1after + " " + chains[i].getCurrentLogLikelihood() + " " + p2after + " " + chains[j].getCurrentLogLikelihood());
+//				System.out.println(chains[i].getTemperature() + " " + chains[j].getTemperature()) ;
+				
+								
+				double logAlpha = p1after - p1before + p2after - p2before;
+				System.err.println(successfullSwaps0 + " " + successfullSwaps + ": " + i + " <--> " + j + ": " + logAlpha +  ": " + ((double) successfullSwaps/totalSwaps));
+				if (Math.exp(logAlpha) < Randomizer.nextDouble()) {
+					// swap fails
+					//assignState(chains[i], chains[j]);
+//					swapStates(chains[i], chains[j]);
+					// not neeeded since the temperature scaler is only applied later
+//					chains[i].calcCurrentLogLikelihoodRobustly();
+//					chains[j].calcCurrentLogLikelihoodRobustly();
+
+					
+				} else {
+					successfullSwaps++;
+					if (i == 0) {
+						successfullSwaps0++;
+					}
+					System.err.println(i + " <--> " + j);
+					swapStates(chains[i], chains[j]);
+					chains[i].calcCurrentLogLikelihoodRobustly();
+					chains[j].calcCurrentLogLikelihoodRobustly();
+
+					//assignState(chains[j], chains[i]);
+					//chains[j].calcCurrentLogLikelihoodRobustly();
+				}
+				totalSwaps++;
+				
+				// tuning
+				for (int k = 1; k < chains.length; k++) {
+					chains[k].optimiseRunTime(startTime, finishTimes[k], finishTimes[0]);
+				}
+			}
+		}
+
+		System.err.println("#Successfull swaps = " + successfullSwaps);
+		System.err.println("#Successfull swaps with cold chain = " + successfullSwaps0);
+		// wait 5 seconds for the log to complete
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// ignore
+		}
+	} // run
+	
+	private void assignState(HeatedMCMC mcmc1, HeatedMCMC mcmc2) {
+		State state1 = mcmc1.startStateInput.get();
+		State state2 = mcmc2.startStateInput.get();
+		List<StateNode> stateNodes1 = state1.stateNodeInput.get();
+		List<StateNode> stateNodes2 = state2.stateNodeInput.get();
+		for (int i = 0; i < stateNodes1.size(); i++) {
+			StateNode stateNode1 = stateNodes1.get(i);
+			StateNode stateNode2 = stateNodes2.get(i);
+			stateNode1.assignFromWithoutID(stateNode2);
+		}
+	}
+
+	/* swaps the states of mcmc1 and mcmc2 */
+	void swapStates(MCMC mcmc1, MCMC mcmc2) {
+		State state1 = mcmc1.startStateInput.get();
+		State state2 = mcmc2.startStateInput.get();
+		
+		List<StateNode> stateNodes1 = state1.stateNodeInput.get();
+		List<StateNode> stateNodes2 = state2.stateNodeInput.get();
+		for (int i = 0; i < stateNodes1.size(); i++) {
+			StateNode stateNode1 = stateNodes1.get(i);
+			StateNode stateNode2 = stateNodes2.get(i);
+			StateNode tmp = tmpStateNodes.get(i);
+			tmp.assignFromWithoutID(stateNode1);
+			stateNode1.assignFromWithoutID(stateNode2);
+			stateNode2.assignFromWithoutID(tmp);
+		}
+	}
+	
+	
+} // class MCMCMC
+
+
+
