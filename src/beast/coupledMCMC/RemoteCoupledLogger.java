6d2c73dab9e457140a5b324fe8f8d43a6b17d5a3
rbouckaert <higgs.ml@gmail.com>
2020-02-03 14:57:26 +1300

    start on remote version -- loggers first

---
+++ src/beast/coupledMCMC/RemoteCoupledLogger.java
@@ -0,0 +1,104 @@
+package beast.coupledMCMC;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Input.Validate;
+import beast.core.util.Log;
+
+@Description("Logger for CoupledMCMC that logs via a RemoteLoggerService")
+class RemoteCoupledLogger extends CoupledLogger {
+	final public Input<String> hostInput = new Input<>("loggerserver", "URL of MC3 logger service", Validate.REQUIRED);	
+	final public Input<Integer> portInput = new Input<>("port", "port MC3 logger service", Validate.REQUIRED);	
+
+	private Socket socket		 = null; 
+	private DataOutputStream out	 = null; 
+	
+	@Override
+	public void initAndValidate() {
+
+		// establish a connection 
+		try
+		{ 
+			String address = InetAddress.getByName(hostInput.get()).getHostAddress();
+			socket = new Socket(address, portInput.get()); 
+			Log.info("Connected to logger service at " + address + ":" + portInput.get()); 
+			// sends output to the socket 
+			out = new DataOutputStream(socket.getOutputStream()); 
+		} 
+		catch(UnknownHostException u) 
+		{ 
+			System.out.println(u); 
+		} 
+		catch(IOException i) 
+		{ 
+			System.out.println(i); 
+		} 
+
+		super.initAndValidate();
+	}
+
+    @Override
+    public void init() throws IOException{
+    	if (fileNameInput.get() != null) {
+    		out.writeUTF(fileNameInput.get());
+    	} else {
+    		out.writeUTF("null");    		
+    	}
+    	
+    	if (!suppressLogging) {
+    	    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    	    final String utf8 = StandardCharsets.UTF_8.name();    	    
+    	    try {
+				m_out = new PrintStream(baos, true, utf8);
+	    		super.init();
+				String data = baos.toString(utf8);
+				out.writeUTF(data);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+    }
+    
+    @Override
+    public void log(long sampleNr){
+    	if (!suppressLogging) {
+    	    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    	    final String utf8 = StandardCharsets.UTF_8.name();    	    
+    	    try {
+				m_out = new PrintStream(baos, true, utf8);
+				super.log(sampleNr);
+				String data = baos.toString(utf8);
+				out.writeUTF(data);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+    }
+	
+    @Override
+    public void close() {
+    	if (!suppressLogging) {
+    	    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    	    final String utf8 = StandardCharsets.UTF_8.name();    	    
+    	    try {
+				m_out = new PrintStream(baos, true, utf8);
+	    		super.close();
+	    		String data = baos.toString(utf8);
+				out.writeUTF(data);
+				out.writeUTF("close");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+    }
+	
+}
---
+++ src/beast/coupledMCMC/RemoteLoggerServer.java
@@ -0,0 +1,122 @@
+package beast.coupledMCMC;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import beast.core.util.Log;
+
+public class RemoteLoggerServer {
+
+	// server is listening on port
+	private ServerSocket ss;
+	private Thread serverThread;
+
+	public RemoteLoggerServer(int port) throws IOException {
+		// server is listening on port
+		ss = new ServerSocket(port);
+
+		serverThread = new Thread() {
+			boolean running = true;
+
+			@Override
+			public void run() {
+				// running infinite loop for getting client requests
+				while (running) {
+					Socket s = null;
+
+					try {
+						// socket object to receive incoming client requests
+						s = ss.accept();
+
+						Log.info("A new client is connected : " + s);
+
+						// obtaining input and out streams
+						DataInputStream dis = new DataInputStream(s.getInputStream());
+
+						// create a new thread object
+						Thread t = new LoggerClientHandler(s, dis);
+
+						// Invoking the start() method
+						t.start();
+
+					} catch (Exception e) {
+						try {
+							s.close();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			@Override
+			public void interrupt() {
+				running = false;
+				super.interrupt();
+			}
+		};
+			
+		serverThread.start();
+
+	}
+
+	public class LoggerClientHandler extends Thread {
+		final DataInputStream dis;
+		final Socket s;
+		PrintStream out = null;
+
+		// Constructor
+		public LoggerClientHandler(Socket s, DataInputStream dis) {
+			this.s = s;
+			this.dis = dis;
+		}
+
+		@Override
+		public void run() {
+			String received;
+
+			// first message indicates the file name
+			try {
+				received = dis.readUTF();
+				if (received.equals("null")) {
+					out = System.out;
+				} else {
+					out = new PrintStream(received);
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			while (true) {
+				try {
+
+					// receive the answer from client
+					received = dis.readUTF();
+					if (received.equals("close")) {
+						break;
+					}
+					out.println(received);
+					out.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			try {
+				// closing resources
+				this.dis.close();
+				// close connections
+				serverThread.interrupt();
+				ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
